c02763f4854dbb95c4901c02f10df7ac2617da66
marjorry <marjorie.s@escolar.ifrn.edu.br>
2025-04-15 11:54:27 -0300

    15/04

--- POO1BIM/Assets/teste.cs
+++ POO1BIM/Assets/teste.cs
@@ -4,6 +4,9 @@ public class teste : MonoBehaviour
 {
     carro fusca = new carro();
     carro gol = new carro();
+    
+    caminhao caminhao = new caminhao();
+    
     void Start()
     {
         fusca. AtribuirNome("Fusca do Shalom");
@@ -12,11 +15,36 @@ public class teste : MonoBehaviour
         fusca.AtribuirPreco(2050.90f);
         Debug.Log(" 0 " + fusca.NomeDoCarro() + " é " + fusca.CorDoCarro() + "tem" + fusca.Numero_de_rodas() +" e custa R$" + fusca.Preco());
 
+        if (fusca.ComparePreco(gol))
+        {
+            Debug.Log("O carro " + fusca.NomeDoCarro() + "é mais caro do que o carro " + gol.NomeDoCarro());
+        }
+        else
+        {
+            Debug.Log("O carro " + fusca.NomeDoCarro() + "é mais barato do que o carro " + gol.NomeDoCarro());
+        }
+        
+        caminhao.AtribuirTipoDeCarroceria("Larga");
+        caminhao.AtribuirNumero_de_rodas(4);
+        caminhao.AtribuirPreco(2050.90f);
+        caminhao.AtribuirCor("Verde");
+        caminhao.AtribuirNome("Fusca do Shalom");
 
-        gol. AtribuirNome("Fusca de marie");
-        gol.AtribuirCor("rosa");
-        gol.AtribuirNumero_de_rodas(4);
-        gol.AtribuirPreco(3000.90f);
+        if (fusca.ComparePreco(caminhao))
+        {
+            Debug.Log("O carro " + fusca.NomeDoCarro() + "é mais caro do que o carro " + gol.NomeDoCarro());
+        }
+        //else
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
     }
