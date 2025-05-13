50a8c6a392ea113fd9a2aec1db7b57440035e74a
marjorry <marjorie.s@escolar.ifrn.edu.br>
2025-05-13 11:30:28 -0300

    movimento do inimigo 2 - 13/05

--- POO1BIM/Assets/movimentodoinimigo.cs
+++ POO1BIM/Assets/movimentodoinimigo.cs
@@ -8,10 +8,14 @@ public class movimentodoinimigo : MonoBehaviour
     private float velocidade;
 
     public float raioDeVisao = 3;
+    private bool naVisao = false;
+    
+    private SphereCollider _sphereCollider;
     void Start()
     {
         velocidade = gameObject.GetComponent<inimigo>().Velocidade();
         _rigidbody = gameObject.GetComponent<Rigidbody>();
+        _sphereCollider = gameObject.GetComponent<SphereCollider>();
         
         _player = GameObject.FindWithTag("Player");
     }
@@ -22,9 +26,17 @@ public class movimentodoinimigo : MonoBehaviour
         if (Vector3.Distance(transform.position, _player.transform.position) < raioDeVisao)
         {
             transform.LookAt(_player.transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, velocidade * Time.deltaTime);
         }
     }
 
+    void OnTriggerStay(Collider colisao)
+    {
+        if (colisao.gameObject.CompareTag("Player"))
+        {
+            naVisao = true;
+        }
+    }
 
    
 }
